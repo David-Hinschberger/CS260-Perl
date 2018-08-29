57d41d5430870b06fa28748896242802140a3fc7
David Hinschberger <david.hinschberger@und.edu>
2018-08-29 13:31:28 -0500

    completed assignment

--- Assignment1.pl
+++ Assignment1.pl
@@ -18,38 +18,36 @@ sub inputchomp(){
     return $input;
 }
 
-# print "Mailing Label Creation (Part 1)\nEnter first name: ";
-# @vals[0] = inputchomp();
-# print "Enter last name: ";
-# @vals[1] = inputchomp();
-# print "Enter street address: ";
-# @vals[2] = inputchomp();
-# print "Enter city: ";
-# @vals[3] = inputchomp();
-# print "Enter state: ";
-# @vals[4] = uc(inputchomp());
-# print "Enter zip code: ";
-# @vals[5] = inputchomp();
+print "Mailing Label Creation (Part 1)\nEnter first name: ";
+@vals[0] = inputchomp();
+print "Enter last name: ";
+@vals[1] = inputchomp();
+print "Enter street address: ";
+@vals[2] = inputchomp();
+print "Enter city: ";
+@vals[3] = inputchomp();
+print "Enter state: ";
+@vals[4] = uc(inputchomp());
+print "Enter zip code: ";
+@vals[5] = inputchomp();
 
-# if(length(@vals[4]) > 2){
-#     @vals[4] = substr(@vals[4], 0, 2);
-# }
+if(length(@vals[4]) > 2){
+    @vals[4] = substr(@vals[4], 0, 2);
+}
 
-# print "\n\n", @vals[0], " ", @vals[1], "\n", @vals[2], "\n", @vals[3], " ", @vals[4]. ", ", @vals[5];
+print "\n\n", @vals[0], " ", @vals[1], "\n", @vals[2], "\n", @vals[3], " ", @vals[4]. ", ", @vals[5];
 
 # Part 2: Making Change
 # Get user input for product name, price, and tendered money
 # print out change counting each amount of dollars, quarters, dimes, nickels, and pennies.
-
-
 
 print "Enter product name: ";
 @vals[6] = inputchomp();
 print "Enter price of the ", lc(@vals[6]), ": ";
 @vals[7] = inputchomp();
 print "Enter the amount of cash offered: ";
-$currMoney = inputchomp() - @vals[7];
-print "Change returned from purhcase of: ", @vals[6], "\nDollars\t", int($currMoney);
-#, "\nQuarters\t", "\nDimes\t", "\nNickels\t", "\nPennies\t", ;
+$currMoney = int(inputchomp() * 100) - int(@vals[7] * 100);
+print "\nChange returned from purhcase of: ", @vals[6], "\nDollars  ", int($currMoney / 100), "\nQuarters ", int($currMoney/250),
+ "\nDimes    ", int($currMoney % 25 / 10), "\nNickels  ", int($currMoney % 25 % 10 / 5), "\nPennies  ", int($currMoney % 25 % 10 % 5);
 
 
