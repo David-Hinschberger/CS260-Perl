593161f60441ac1428da7deadeb259b75aaf9589
David Hinschberger <david.hinschberger@und.edu>
2018-08-29 09:38:51 -0500

    first

---
+++ Assignment1.pl
@@ -0,0 +1,55 @@
+# David Hinschberger
+# David.Hinschberger@und.edu
+# Program 1
+# Part 1: Working with Text
+# Allow the user to enter their first name, last name, address, city, state and zip code. The
+# program should ask for one input at a time. Print out a mailing label as follows:
+# First Last
+# Address
+# City STATE, Zip code
+
+use strict;
+
+my($input, @vals, $currMoney);
+
+sub inputchomp(){
+    $input = <STDIN>;
+    chomp $input;
+    return $input;
+}
+
+# print "Mailing Label Creation (Part 1)\nEnter first name: ";
+# @vals[0] = inputchomp();
+# print "Enter last name: ";
+# @vals[1] = inputchomp();
+# print "Enter street address: ";
+# @vals[2] = inputchomp();
+# print "Enter city: ";
+# @vals[3] = inputchomp();
+# print "Enter state: ";
+# @vals[4] = uc(inputchomp());
+# print "Enter zip code: ";
+# @vals[5] = inputchomp();
+
+# if(length(@vals[4]) > 2){
+#     @vals[4] = substr(@vals[4], 0, 2);
+# }
+
+# print "\n\n", @vals[0], " ", @vals[1], "\n", @vals[2], "\n", @vals[3], " ", @vals[4]. ", ", @vals[5];
+
+# Part 2: Making Change
+# Get user input for product name, price, and tendered money
+# print out change counting each amount of dollars, quarters, dimes, nickels, and pennies.
+
+
+
+print "Enter product name: ";
+@vals[6] = inputchomp();
+print "Enter price of the ", lc(@vals[6]), ": ";
+@vals[7] = inputchomp();
+print "Enter the amount of cash offered: ";
+$currMoney = inputchomp() - @vals[7];
+print "Change returned from purhcase of: ", @vals[6], "\nDollars\t", int($currMoney);
+#, "\nQuarters\t", "\nDimes\t", "\nNickels\t", "\nPennies\t", ;
+
+
---
+++ class1.pl
@@ -0,0 +1,15 @@
+# use strict;
+# use warnings; 
+
+
+#$x = <STDIN>;
+
+#print STDOUT "Hello, World!";
+
+#$foo = q{apple orange pineapple};
+
+# $date = 'date /T';
+
+# print "$date";
+print "$poop";
+print "idk";
