69e57a94cbe44e7fc2595d7eaef98949f28ed246
David Hinschberger <david.hinschberger@und.edu>
2018-09-10 22:56:37 -0500

    assignment 2 working 19lines

--- Assignment2.pl
+++ Assignment2.pl
@@ -7,14 +7,13 @@ sub inputchomp(){
 
 print "Enter the student's name: ";
 my $name = inputchomp;
-my (@credits, @grades, @passed);
+my (@credits, @grades, @passed, $honorPts);
 
 while(1){
     print "\nEnter a class: ";
     inputchomp eq '' ? last : print "Enter the number of credits: ";
-    push @credits, inputchomp;
-    print "Enter the grade received: ";
-    push @passed, @grades[(push @grades, inputchomp)-1] =~ /[ABCDabcd]/ ? $credits[-1] : 0;
+    (push @credits, inputchomp) > 0 ? print "Enter the grade received: " : 0;
+    push @passed, @grades[(push @grades, uc(inputchomp))-1] =~ /[A-D]/ ? @grades[-1] eq 'A'? $credits[-1] + $honorPts + 4 - ($honorPts += 4) : @grades[-1] eq 'B' ? $credits[-1] + $honorPts + 3 - ($honorPts += 3) : @grades[-1] eq 'C' ? $credits[-1] + $honorPts + 2 - ($honorPts += 2) : @grades[-1] eq 'D'? $credits[-1] + $honorPts - $honorPts++: 0 : 0;
 }
 
-printf "Transcript for $name\nCredits Taken: %9d\nCredits Passed: %8d\nSemester GPA: %9.2f", (eval join '+', @credits), (eval join '+', @passed), ;
+printf "\nTranscript for $name\nCredits Taken: %11d\nCredits Passed: %10d\nSemester GPA: %12.2f", (eval join '+', @credits), (eval join '+', @passed), (scalar @grades) == 0? 0 : $honorPts / scalar @grades;
--- test.pl
+++ test.pl
@@ -1,2 +1,5 @@
 use strict;
 
+my $foo = 0;
+my $bar = $foo++;
+print $bar;
