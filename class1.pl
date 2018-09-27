bb0eb0f6b292ac2a76c0ee3f65aaf47b24d5676a
David Hinschberger <david.hinschberger@und.edu>
2018-09-27 15:05:02 -0500

    open(cmd)

--- class1.pl
+++ class1.pl
@@ -1,9 +1,27 @@
 use strict;
-use warnings; 
+use Data::Dumper;
 
-my (%honorPoints, $key, $value);
-%honorPoints = ('A', 4, B => 3, C => 2, D => 1, F => 0);
+my ($header, $line, @scores, $id, $name, %data1, %data2, %data3);
 
-foreach my $val (sort {$honorPoints{$a} <=> $honorPoints{$b} or $a cmp $b} keys %honorPoints){
-    printf "%-8s %s\n", $val, $honorPoints{$val};
+my $result = open (CSVFILE, "Book11.csv");
+if (!$result){
+    print "unable to find data file.\n";
+    exit();
 }
+
+chomp ($header = <CSVFILE>);
+while(chomp ($line = <CSVFILE>)){
+    my @scores;
+    #print "$line\n";
+    ($name, $id, @scores) = split (",", $line);
+    #print "$name $id\n";
+    #data1 associates the id with the scores
+    unshift @scores, $name;
+    $data1{$id} = \@scores;
+    #data2 associates the id with the name and the scores
+}
+
+#print "Final score for student with id of 2 ", $data1{2}->[3];
+print Dumper (\%data1), "\n";
+
+close CSVFILE;
--- test.pl
+++ test.pl
@@ -1,37 +1,16 @@
-use strict;
-use Data::Dumper;
+#use strict;
 
-sub showText{
-    my %params = (
-        text => '',
-        myFont => "Arial",
-        ptSize => 10,
-        bold => 0,
-        italics => 0,
-    );
-    if (scalar(@_) == 0){ return; } #do we have at least one argument?
-    $params{text} = shift; #remove the required positinal parameter
-    #second positional parameter
-    if (@_ && substr ($_[0], 0, 1) ne "-") { #is the first character of hte first index in @_ a '-'
-        { $params {myFont} = shift }
-    }
-    while(@_) {
-        my ($key, $value) = shift, shift;
-        if (defined (%params{substr ($key, 1)})){
-            $key = substr ($key, 1);
-            $key = substr ($key, 1);
-            if ($value) {$params {$key} = $value; }
-        }
-        else { print "Ukown key $key : ignoring value of $value\n" }
-    }
-    print Dumper (\%params), "\n";
-}
+my ($perlFile);
 
-showText("All default values for parameters");
-showText("no arguments", "Courier");
-showText("test1", -bold => 'True');
-showText("test2", -ptSize => 12, -myFont => "Arial", );
-showText("test2b", -myFont => "Arial", -ptSize => 12, -bold => 'True');
-showText("test3", -myFont => "Courier", -ptSize => 12);
-showText("test4", -ptSize => 20);
-showText("test5", -ptSize => 20, -myFont => "Times New Roman", -italics => 'True');
+$perlFile = $ARGV[0];
+
+print "filename - $perlFile\n";
+my $foo = `perl -c $perlFile 2>&1 1>nul`;
+#open (PERLOUTPUT, "-|", "perl -c $perlFile 2>&1");
+#my $foo = <PERLOUTPUT>;
+#$foo = <PERLOUTPUT>;
+chomp $foo; 
+
+print ">>$foo<<";
+
+close PERLOUTPUT;
