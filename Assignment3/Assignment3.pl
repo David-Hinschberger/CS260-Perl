66dd2e4ba7ce3fc4dc64673519a19353b131edb2
David Hinschberger <david.hinschberger@und.edu>
2018-10-01 23:13:23 -0500

    Assignment3 finished

--- Assignment3/Assignment3.pl
+++ Assignment3/Assignment3.pl
@@ -1,13 +1,36 @@
+#!/usr/bin/perl
+# David Hinschberger 1126026
+# CS260 Perl Fall 2018
+# Assignment 3: Perl Module 
 use strict;
 use lib '.';
 use ListUtil;
 use Data::Dumper;
 
-addItem("Bob");
-addItem("Bob");
-my @list = list();
+print name("names"), "\n";
+print name(), "\n";
+print "add Bob: ", addItem("Bob"), "\n";
+print "add Cory:", addItem("Cory"), "\n";
+print "add AJ:", addItem("AJ"), "\n";
+print "add Sam:", addItem("Sam"), "\n";
+print "add Pam:", addItem("Pam"), "\n";
+print "add Bob:", addItem("Bob"), "\n";
 
+print Dumper list();
 
-print(removeItem("Bob"));
+print 'remove Bob: ', removeItem("Bob"), "\n";
+print 'remove Bob: ', removeItem("Bob"), "\n";
 
-print(removeItem("Jim"));
+isInList("Bob") ? print "bob in list\n" : print "bob not in list\n";
+
+isInList("Pam") ? print "pam in list\n" : print 'pam not in list', "\n";
+
+print 'save to file "liststuff.txt": ', saveToFile("liststuff.txt"), "\n";
+
+print 'load from file "liststuff.txt": ', loadFromFile(), "\n";
+
+print Dumper list();
+
+print 'count: ', count(), "\n";
+
+print list('-type', 'array');
--- Assignment3/ListUtil.pm
+++ Assignment3/ListUtil.pm
@@ -1,59 +1,90 @@
 #!/usr/bin/perl
-# David Hinschberger
+# David Hinschberger 1126026
 # CS260 Perl Fall 2018
 # Assignment 3: Perl Module 
 package ListUtil;
 use strict;
-
-our $version = 0.01;
 use Exporter;
+our $version = 0.01;
 our @ISA = qw/Exporter/;
-our @EXPORT = qw (&name &addItem &removeItem &isInList &saveToFile &loadToFile &count &list $version);
+our @EXPORT = qw (&name &addItem &removeItem &isInList &saveToFile &loadFromFile &count &list $version);
 
 my @list;
-my $listName = '';
+my $listName = 'aa';
 
 sub name{
     my $arg = shift;
-    unless(defined($arg)){
-        return $listName;
-    }
-    $listName = $arg;
+    defined $arg? $listName = $arg : return $listName;
     return;
 }
 
 sub addItem{
     my $arg = shift;
-    unless (defined $arg xor grep{$_ eq $arg} @list){
-        return 0;
-    }
-    push @list, $arg;
-    return 1;
+    (defined $arg and not isInList($arg)) ? return ((push @list, $arg) + 1 - scalar @list) : return 0;
 }
 
 sub removeItem{
     my $arg = shift;
-    return (defined(splice(@list, $arg, 1)));
+    my $index = 0;
+    $index++ until $list[$index] eq $arg or $index == scalar @list;
+    return defined((splice(@list, $index, 1)));
 }
 
 sub isInList{
-
+    my $arg = shift;
+    return grep{$arg eq $_} @list;
 }
 
 sub saveToFile{
-
-
+    my $fileName = shift;
+    unless(defined $fileName){
+        return 0;
+    }
+    open (SAVEFILE, '>', "$fileName") or die "could not save to $fileName!";
+    print SAVEFILE "$listName\n";
+    foreach my $i (0..@list-1){
+        print SAVEFILE "$list[$i]\n";
+    }
+    print SAVEFILE @list[-1];
+    close SAVEFILE;
+    return 1;
 }
 
-sub loadToFile{
-
+sub loadFromFile{
+    my $fileName = shift;
+    unless (defined $fileName){
+        return 0;
+    }
+    open (LOADFILE, "$fileName") or die "could not load from $fileName!";
+    my $line = <LOADFILE>;
+    $listName = $line;
+    undef @list;
+    while($line = <LOADFILE>){
+        chomp $line;
+        push @list, $line;
+    }
+    return 1;
 }
+
 sub count{
-
+    return scalar @list;
 }
 
 sub list{
-    return \@list;
+    my $arg = shift;
+    unless(defined $arg){
+        return \@list;
+    }
+    if($arg eq '-type'){
+        my $typeToReturn = shift;
+        if($typeToReturn eq 'array'){
+            return @list;
+        }
+        if($typeToReturn eq 'ref'){
+            return \@list;
+        }
+    }
+    return 0;
 }
 
 return 1
