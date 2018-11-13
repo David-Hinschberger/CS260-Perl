b15134c2579b7ade926ebaf13c3bc4e045f3d759
David Hinschberger <david.hinschberger@und.edu>
2018-11-13 11:12:13 -0600

    Assignment4 17lines

--- Assignment4/Assignment4.pl
+++ Assignment4/Assignment4.pl
@@ -7,10 +7,10 @@ sub inputchomp() {
     chomp ($_ = <STDIN>) == 1 ? return $_ : print 'chomp removed more than 1 \n somehow';
 }
 my (@data, $foo, @legend, @search, $searchstr);
-my $fileName = ($ARGV[0] && print "Input file: $ARGV[0]\n") ? $ARGV[0] : (print "Input file: ") ? inputchomp : 0;
+my $fileName = ($ARGV[0] && ($foo = 1) && print "Input file: $ARGV[0]\n") ? $ARGV[0] : (print "Input file: ") ? inputchomp : 0;
 print "Could not read file: $fileName" and exit unless (defined open(INPUT, $fileName));
-push @data, $_ while(chomp($_ = <INPUT>) >= 0 && $_);
-while(($searchstr = (print "Search: ") ? '' : '') or ((@legend = qw([0] [1] [2ABC] [3DEF] [4GHI] [5JKL] [6MNO] [7PQRS] [8TUV] [9WXYZ])) and ($fileName = -1))) {
+((push @data, $_) && ($foo-- > 0 && (@legend = qw([0] [1] [2ABC] [3DEF] [4GHI] [5JKL] [6MNO] [7PQRS] [8TUV] [9WXYZ])))) while(chomp($_ = <INPUT>) >= 0 && $_);
+while(((print"Search: ") && (($searchstr = '') != ($fileName = -1)))) {
     $searchstr .= (($foo = $_) && (@_ = grep(/$foo/i, @legend))[0] . '+.*') foreach (((@search = split (undef , inputchomp )) and (grep(/[^A-Z0-9]/i, @search)))? last : ((scalar @search == 0))? last : @search);
     (grep (/$searchstr/i, (split /:/,$data[$fileName]))) && (printf "%-20s%14s\n", (split (/:/,$data[$fileName]))[0], length($foo = join('', split /[^\d]/, ((split (/:/,$data[$fileName])))[1])) == 11? (substr($foo, 1, 0) = '-' && substr($foo, 4, 0) = '-' && substr($foo, 7, 0) = '-') ? $foo : 'err1' : (length($foo = join('', '1-', split /[^\d]/, ((split (/:/,$data[$fileName]))[1]))) == 12)? (substr($foo, 5, 0) = '-' && substr($foo, 8,0) = '-' ) ? $foo : 'err2' : (length($foo = join('', '1-701-', split /[^\d]/, (split (/:/,$data[$fileName]))[1])) == 13)? (substr($foo, 9, 0) = '-' )? $foo : 'err3' : 'INVALID/CORRUPT PHONE NUMBER') while(++$fileName < scalar(@data));
 }
--- test.pl
+++ test.pl
@@ -1,4 +1,15 @@
-#use strict;
-my $foo = 'bar';
-print --$foo;
-print --$foo;
+use Data::Dumper;
+
+sub printGoodGrades{
+    %hash = (@_[0]);
+    print Dumper(\%hash);
+}
+
+%grades = ('Luke' => 100,
+            'Adam' => 60,
+            'Bob' => 70,
+            'Sam' => 80,
+            'Cory' => 90,
+            'AJ' => 50);
+
+printGoodGrades \%grades;
